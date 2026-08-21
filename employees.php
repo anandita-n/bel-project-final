e6e19840934931eec482930f7967b7040d12e644
BEL PMS <bel-pms@localhost>
2026-08-22 00:46:27 +0530

    Revert "Center the Staff department table instead of letting it stretch edge-to-edge"
    This reverts commit 2bc452701bb2efc185846a1ccaa3bfe76ce84207.

--- employees.php
+++ employees.php
@@ -68,7 +68,6 @@ require 'includes/layout_top.php';
 <?php endif; ?>
 
 <?php else: ?>
-<div class="list-page-wrap">
 <div class="breadcrumb"><a href="employees.php">Staff</a> / <?= htmlspecialchars($department) ?></div>
 
 <div class="panel panel-table">
@@ -108,7 +107,6 @@ require 'includes/layout_top.php';
     </div>
 </div>
 <div id="employeesPagination" class="pagination-bar" style="<?= $totalPages <= 1 ? 'display:none;' : '' ?>"></div>
-</div>
 <?php endif; ?>
 
 <script>
